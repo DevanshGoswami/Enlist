98ca22269e626610ba03c2c755a8222519020175
Devansh Goswami <devansh51100@gmail.com>
2020-11-27 22:13:02 +0530

    mail

--- app.js
+++ app.js
@@ -183,7 +183,7 @@ app.get('/forgot', function(req, res) {
         var mailOptions = {
           to: user.username,
           from: 'codechefsrm@gmail.com',
-          subject: 'Node.js Password Reset',
+          subject: 'Recruitment Portal Password Reset',
           text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
             'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
             'http://' + req.headers.host + '/reset/' + token + '\n\n' +
